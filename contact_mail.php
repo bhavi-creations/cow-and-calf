62d10139ee6e4f6d040c029cfa1c674ca21e1447
Ramesh <rameshpilli1428@gmail.com>
2024-02-07 18:34:59 +0530

    mail intergation and links updated

---
+++ contact_mail.php
@@ -0,0 +1,55 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Adjust the path to autoload.php based on your project
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Assign POST data to variables
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+     $message = $_POST['comments'] ?? '';
+
+     
+    // Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings for Gmail SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'cowcaulf@gmail.com'; // Your Gmail email address
+        $mail->Password = 'ilzyawyagtrsnzvd'; // Your Gmail password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Recipients
+        $mail->setFrom('cowcaulf@gmail.com', 'Cow & Calf'); // Your Gmail email and name
+        $mail->addAddress('cowcaulf@gmail.com', 'Cow & Calf'); // Recipient's email and name
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Message from Contact Form';
+        $mail->Body = "
+            <h1>New Message</h1>
+            <p><strong>Name:</strong> $name</p>
+            <p><strong>Email:</strong> $email</p>
+
+            <p><strong>Phone:</strong> $phone</p>
+           
+            <p><strong>Message:</strong><br>$message</p>
+        ";
+
+        $mail->send();
+        echo '<script> window.alert("Message has been sent.\n\nPlease click OK."); window.location.href="index.html";</script>';
+
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+} else {
+    // If accessed directly without POST data
+    echo 'Access Denied';
+}
